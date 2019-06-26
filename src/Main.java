536f7f0b56b320dca3dee7e00e894b5d228b8174
RealgodJJ <xiaozhiqiu13@sina.com>
2019-06-26 15:02:53 +0800

    Test time complexity.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
-public class Main {
+import java.util.Random;
 
+public class Main {
+    private static double testQueue(Queue<Integer> queue, int opCount) {
+        long startTime = System.nanoTime();
+
+        Random random = new Random();
+        for (int i = 0; i < opCount; i++)
+            queue.enqueue(random.nextInt(Integer.MAX_VALUE));
+        for (int i = 0; i < opCount; i++)
+            queue.dequeue();
+
+        long endTime = System.nanoTime();
+
+        return (endTime - startTime) / 1000000000.0;
+    }
 
     public static void main(String[] args) {
         ArrayQueue<Integer> arrayQueue = new ArrayQueue<>();
@@ -23,5 +37,14 @@ public class Main {
 
             System.out.println(loopQueue);
         }
+
+        int opCount = 100000;
+        ArrayQueue<Integer> arrayQueue1 = new ArrayQueue<>();
+        double time_1 = testQueue(arrayQueue1, opCount);
+        System.out.println("ArrayQueue Time: " + time_1 + "s");
+
+        LoopQueue<Integer> loopQueue1 = new LoopQueue<>();
+        double time_2 = testQueue(loopQueue1, opCount);
+        System.out.println("LoopQueue Time: " + time_2 + "s");
     }
 }
